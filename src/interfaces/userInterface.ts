739dfeee1160f58a012bb0bcd16b9c8ba7b4c9c7
Julien BERTRAND <j.bertrand.sin@gmail.com>
2024-01-24 11:18:23 +0100

    feat: Implementation mcd

--- src/interfaces/userInterface.ts
+++ src/interfaces/userInterface.ts
@@ -11,8 +11,19 @@ export interface User {
   firstName?: string;
   lastName?: string;
   groupsId?: number;
-  languagePreference?: string;
   group?: Groups;
+  languagePreference?: string;
+  localisation?: string;
+  titles?: string;
+  badges?: string;
+  company?: string;
+  url?: string;
+  school?: string;
+  github?: string;
+  Histories?: Histories;
+  userRanking?: UserRanking;
+  userTournament?: UserTournament;
+  userMatch?: UserMatch;
 }
 
 export interface Groups {
@@ -63,4 +74,77 @@ export interface UserMail {
   firstName?: string;
   lastName?: string;
   groupsId?: number;
+}
+
+interface Histories {
+  id: number;
+  userID: number;
+  user: User;
+  modificationType: string;
+  details: string;
+  modificationDate: Date;
+  oldValue: string;
+  newValue: string;
+}
+
+interface Tournament {
+  id: number;
+  startDate: Date;
+  endDate: Date;
+  playerMax: number;
+  title: string;
+  description: string;
+  rewards: string;
+  matches: Match[];
+  userTournaments: UserTournament[];
+}
+
+interface Match {
+  id: number;
+  date: Date;
+  time: Date;
+  location: string;
+  status: string;
+  score: number;
+  tournamentID: number;
+  tournament: Tournament;
+  rankingsID: number;
+  rankings: Ranking;
+}
+
+interface Ranking {
+  id: number;
+  startDate: Date;
+  endDate: Date;
+  title: string;
+  description: string;
+  rewards: string;
+  matches: Match[];
+  userRanking: UserRanking[];
+}
+
+interface UserRanking {
+  id: number;
+  userID: number;
+  user: User;
+  rankingsID: number;
+  rankings: Ranking;
+  points: number;
+}
+
+interface UserTournament {
+  id: number;
+  userID: number;
+  user: User;
+  tournamentID: number;
+  tournament: Tournament;
+  points: number;
+}
+
+interface UserMatch {
+  id: number;
+  userID: number;
+  user: User;
+  matchID: number;
+  match: Match;
 }
