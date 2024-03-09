56c4ce0d05d1420cf71c6440589f3609dd8b6037
Julien BERTRAND <j.bertrand.sin@gmail.com>
2024-03-09 16:03:49 +0100

    fix: update prisma schemas

--- src/controlleur/tournament/tournament.controller.ts
+++ src/controlleur/tournament/tournament.controller.ts
@@ -1,79 +1,89 @@
-import {Body, Controller, Delete, Get, HttpException, HttpStatus, Post, Query, Res, UseGuards,} from '@nestjs/common';
-import {TournamentService} from '../../services/tournament/TournamentService';
-import {Tournament, UserTournament} from '../../interfaces/userInterface';
-import {ADMIN, ENTREPRISE, USER} from '../../constantes/contante';
-import {RolesGuard} from '../../guards/roles.guard';
-import {Roles} from '../auth/auth.controller';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+  Query,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { TournamentService } from '../../services/tournament/TournamentService';
+import { Tournament, UserTournament } from '../../interfaces/userInterface';
+import { ADMIN, ENTREPRISE, USER } from '../../constantes/contante';
+import { RolesGuard } from '../../guards/roles.guard';
+import { Roles } from '../auth/auth.controller';
 
 @Controller('tournament')
 export class TournamentController {
-    constructor(private readonly tournamentService: TournamentService) {
-    }
+  constructor(private readonly tournamentService: TournamentService) {}
 
-    @Get('/findNextTenTournament')
-    @Roles(USER, ADMIN, ENTREPRISE)
-    @UseGuards(RolesGuard)
-    async findNextTenTournament(@Res() response) {
-        try {
-            const getAllTournaments: Tournament[] =
-                await this.tournamentService.findNextTenTournament();
-            if (getAllTournaments) {
-                response.send(getAllTournaments);
-            } else {
-                response.send(HttpStatus.NOT_FOUND);
-            }
-        } catch (error) {
-            console.log(error);
-        }
+  @Get('/findNextTenTournament')
+  @Roles(USER, ADMIN, ENTREPRISE)
+  @UseGuards(RolesGuard)
+  async findNextTenTournament(@Res() response) {
+    try {
+      const getAllTournaments: Tournament[] =
+        await this.tournamentService.findNextTenTournament();
+      if (getAllTournaments) {
+        response.send(getAllTournaments);
+      } else {
+        response.send(HttpStatus.NOT_FOUND);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    @Get('/findTournament')
-    @Roles(USER, ADMIN, ENTREPRISE)
-    @UseGuards(RolesGuard)
-    async findTournament(@Query('id') id: string, @Res() response) {
-        try {
-            const getTournament: Tournament =
-                await this.tournamentService.findTournament(id);
-            if (getTournament) {
-                response.send(getTournament);
-            } else {
-                response.send(HttpStatus.NOT_FOUND);
-            }
-        } catch (error) {
-            console.log(error);
-        }
+  @Get('/findTournament')
+  @Roles(USER, ADMIN, ENTREPRISE)
+  @UseGuards(RolesGuard)
+  async findTournament(@Query('id') id: string, @Res() response) {
+    try {
+      const getTournament: Tournament =
+        await this.tournamentService.findTournament(id);
+      if (getTournament) {
+        response.send(getTournament);
+      } else {
+        response.send(HttpStatus.NOT_FOUND);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    @Post('/inscription')
-    @Roles(USER)
-    @UseGuards(RolesGuard)
-    async update(@Body() data, @Res() response) {
-        const user: UserTournament = data.data;
-        console.log(user);
-        const res: HttpStatus = await this.tournamentService.update(user);
-        if (res === HttpStatus.CREATED) {
-            response.send(res);
-        } else if (res === HttpStatus.NOT_ACCEPTABLE) {
-            // Si la mise à jour échoue, on envoie une exception HTTP avec un code 406
-            throw new HttpException('Utilistaeur inconnu', HttpStatus.NOT_ACCEPTABLE);
-        } else {
-            // Si la mise à jour échoue, on envoie une exception HTTP avec un code 400
-            throw new HttpException('Utilistaeur inconnu', HttpStatus.BAD_REQUEST);
-        }
+  @Post('/inscription')
+  @Roles(USER)
+  @UseGuards(RolesGuard)
+  async update(@Body() data, @Res() response) {
+    const user: UserTournament = data.data;
+    console.log(user);
+    const res: HttpStatus = await this.tournamentService.update(user);
+    if (res === HttpStatus.CREATED) {
+      response.send(res);
+    } else if (res === HttpStatus.NOT_ACCEPTABLE) {
+      // Si la mise à jour échoue, on envoie une exception HTTP avec un code 406
+      throw new HttpException('Utilistaeur inconnu', HttpStatus.NOT_ACCEPTABLE);
+    } else {
+      // Si la mise à jour échoue, on envoie une exception HTTP avec un code 400
+      throw new HttpException('Utilistaeur inconnu', HttpStatus.BAD_REQUEST);
     }
+  }
 
-    @Delete('/unsubscribe')
-    @Roles(USER)
-    @UseGuards(RolesGuard)
-    async delete(@Body() user: UserTournament): Promise<HttpException> {
-        const response: HttpStatus =
-            await this.tournamentService.deleteUserTournament(user);
-        if (response === HttpStatus.OK) {
-            // Si la mise à jour réussi, on envoie un code HTTP 200.
-            return new HttpException('Utilistaeur mis à jour', HttpStatus.OK);
-        } else {
-            // Si la mise à jour échoue, on envoie une exception HTTP avec un code 400
-            throw new HttpException('Utilistaeur inconnu', HttpStatus.NOT_FOUND);
-        }
+  @Delete('/unsubscribe')
+  @Roles(USER)
+  @UseGuards(RolesGuard)
+  async delete(@Body() user: UserTournament): Promise<HttpException> {
+    const response: HttpStatus =
+      await this.tournamentService.deleteUserTournament(user);
+    if (response === HttpStatus.OK) {
+      // Si la mise à jour réussi, on envoie un code HTTP 200.
+      return new HttpException('Utilistaeur mis à jour', HttpStatus.OK);
+    } else {
+      // Si la mise à jour échoue, on envoie une exception HTTP avec un code 400
+      throw new HttpException('Utilistaeur inconnu', HttpStatus.NOT_FOUND);
     }
+  }
 }
