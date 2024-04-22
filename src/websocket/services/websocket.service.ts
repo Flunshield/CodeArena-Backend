7515df926d3f74a4f1651328b615f76eb749908d
Kévin Begot <begot.kevin@gmail.com>
2024-04-22 21:58:54 +0200

    [ADD] websocket service for matchmaking (WIP)

--- src/websocket/services/websocket.service.ts
+++ src/websocket/services/websocket.service.ts
@@ -1,8 +1,37 @@
+// matchmaking.service.ts
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { UserRanking } from '../../interfaces/userInterface';
+import { WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @Injectable()
 export class WebsocketService {
-  sendMessage(): string {
-    return 'Hello, World!';
+  @WebSocketServer() server: Server;
+
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async findMatchesForPlayer(playerId: number): Promise<UserRanking[]> {
+    // Récupérer le classement du joueur actuel
+    const currentPlayerRanking = await this.prisma.userRanking.findUnique({
+      where: { id: playerId },
+    });
+
+    if (!currentPlayerRanking) {
+      throw new Error('Player ranking not found');
+    }
+
+    // Récupérer les joueurs avec le même rankingsID, exclure le joueur actuel
+    const potentialMatches = await this.prisma.userRanking.findMany({
+      where: {
+        rankingsID: currentPlayerRanking.rankingsID,
+        NOT: { id: playerId },
+      },
+    });
+
+    // Émettre les correspondances via les websockets
+    this.server.emit('matches', potentialMatches);
+
+    return potentialMatches as UserRanking[];
   }
 }
