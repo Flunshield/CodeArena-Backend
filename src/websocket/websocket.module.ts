fbf652c93cd9a201c361c532096a1d6b5fe1ad01
Kévin Begot <begot.kevin@gmail.com>
2024-05-14 21:55:52 +0200

    [ADD] gateway in module

--- src/websocket/websocket.module.ts
+++ src/websocket/websocket.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WebsocketService } from './services/websocket.service';
 import { WebSocketController } from './controllers/websocket.controller';
+import { PrismaClient } from '@prisma/client';
+import { GatewayService } from './services/gateway.service';
 
 @Module({
-  providers: [WebsocketService],
+  providers: [WebsocketService, PrismaClient, GatewayService],
   controllers: [WebSocketController],
 })
 export class WebsocketModule {}
