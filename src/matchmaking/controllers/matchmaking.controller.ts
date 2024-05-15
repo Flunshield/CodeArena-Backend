4cf33d7d283592ddf1b9b73a560db9386aa2f0c7
Kévin Begot <begot.kevin@gmail.com>
2024-05-15 22:02:07 +0200

    [ADD] message and error in matchmaking controller

--- src/matchmaking/controllers/matchmaking.controller.ts
+++ src/matchmaking/controllers/matchmaking.controller.ts
@@ -12,14 +12,14 @@ import { MatchmakingService } from '../services/matchmaking.service';
 export class MatchmakingController {
   constructor(private readonly matchmakingService: MatchmakingService) {}
 
-  // Endpoint pour ajouter un utilisateur à la file d'attente
   @Post('joinQueue')
   async joinQueue(@Body() userId: { id: number }) {
-    console.log('Received userId:', userId); //TODO : log
     try {
-      // Ajouter l'utilisateur à la file d'attente en utilisant le service
       this.matchmakingService.addToQueue(userId.id);
-      return { message: 'Join queue request received' }; // Une réponse de confirmation par exemple
+      return {
+        success: true,
+        message: 'You have successfully joined the queue.',
+      };
     } catch (error) {
       throw new HttpException(
         `Failed to join queue: ${error.message}`,
@@ -28,43 +28,46 @@ export class MatchmakingController {
     }
   }
 
-  // Endpoint pour trouver un match pour un utilisateur
   @Post('findMatch')
   async findMatch(@Body() userId: { id: number }) {
     try {
-      // Vérifie si l'utilisateur est dans la file d'attente
       const isInQueue = await this.matchmakingService.isUserInQueue(userId.id);
 
       if (!isInQueue) {
-        console.log(`User ${userId.id} is not in the queue`); //TODO : log
         throw new HttpException(
-          'You are not in the queue',
+          'You’re not in the queue.',
           HttpStatus.BAD_REQUEST,
         );
       }
 
-      // Envoie seulement l'ID de l'utilisateur au service
       const match = await this.matchmakingService.findMatch(userId.id);
       if (match !== undefined && match !== userId.id) {
-        return match;
+        return {
+          success: true,
+          matchId: match,
+          message: 'You found a match.',
+        };
       } else if (match === userId.id) {
-        return { message: 'You cannot match with yourself' };
+        throw new HttpException(
+          'You cannot associate with yourself.',
+          HttpStatus.BAD_REQUEST,
+        );
       } else {
-        throw new HttpException('No match found', HttpStatus.NOT_FOUND);
+        throw new HttpException('No matches found.', HttpStatus.NOT_FOUND);
       }
     } catch (error) {
       throw new HttpException(
-        `Failed to find match: ${error.message}`,
+        `Failed to find a match: ${error.message}`,
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
 
-  // Endpoint pour obtenir la liste des utilisateurs dans la file d'attente
   @Get('queue')
   getQueue() {
     try {
-      return this.matchmakingService.getQueue();
+      const queue = this.matchmakingService.getQueue();
+      return { success: true, queue };
     } catch (error) {
       throw new HttpException(
         `Failed to get queue: ${error.message}`,
