bd8ba9e3eb1435c53321f0422e840aa03cb35c0d
Kévin Begot <begot.kevin@gmail.com>
2024-06-07 00:13:21 +0200

    [AUPDATE] path for matchmaking test

--- src/matchmaking/test/matchmaking.controller.spec.ts
+++ src/matchmaking/test/matchmaking.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { MatchmakingController } from './matchmaking.controller';
+import { MatchmakingController } from '../controllers/matchmaking.controller';
 import { MatchmakingService } from '../services/matchmaking.service';
 import { HttpException, HttpStatus } from '@nestjs/common';
 
