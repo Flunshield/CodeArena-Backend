a5d3646b1de7a01dac5578934c74c2fdf61eb787
Kévin Begot <begot.kevin@gmail.com>
2024-09-26 08:51:56 +0200

    [FIX] migration

--- prisma/migrations/20240925161839_alter_puzzles_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `example` to the `puzzles` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `puzzles` ADD COLUMN `example` VARCHAR(191) NOT NULL;
